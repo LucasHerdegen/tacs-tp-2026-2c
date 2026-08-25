77bfe3649f88ae595eb75fe1f9c469b48d7fc2fb
joaquingandola <joaquingandola@gmail.com>
2026-08-25 12:58:25 -0300

    feat: enpoints y service para ver actividades organizadas y participadas: US12

--- backend/src/main/java/com/tacs/backend/repositories/ActividadesRepository.java
+++ backend/src/main/java/com/tacs/backend/repositories/ActividadesRepository.java
@@ -1,8 +1,16 @@
 package com.tacs.backend.repositories;
 
 import com.tacs.backend.domain.actividad.Actividad;
+import com.tacs.backend.domain.actividad.TipoEstadoActividad;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ActividadesRepository extends JpaRepository<Actividad, Long>
 {
+  List<Actividad> findByOrganizadorId(Long organizadorId);
+  List<Actividad> findByOrganizadorIdAndEstadoTipo(Long organizadorId, TipoEstadoActividad tipo);
+  List<Actividad> findByParticipantesId(Long usuarioId);
+  List<Actividad> findByParticipantesIdAndEstadoTipo(Long usuarioId, TipoEstadoActividad tipo);
+
 }
