ce1c28221e8fbb540980650b46b78cfb51651b67
Juanter134 <juancruzzarate0@gmail.com>
2026-08-29 22:38:58 -0300

    feat(votaciones): job de cierre de votaciones

--- backend/src/main/java/com/tacs/backend/repositories/VotacionesRepository.java
+++ backend/src/main/java/com/tacs/backend/repositories/VotacionesRepository.java
@@ -3,6 +3,7 @@ package com.tacs.backend.repositories;
 import com.tacs.backend.domain.votacion.Votacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,4 +11,5 @@ public interface VotacionesRepository extends JpaRepository<Votacion, Long> {
     List<Votacion> findByAbiertaAndActividadOrganizadorId(boolean abierta, Long organizadorId);
     List<Votacion> findByAbiertaAndActividadParticipantesId(boolean abierta, Long usuarioId);
     Optional<Votacion> findByAbiertaTrueAndActividadId(Long actividadId);
+    List<Votacion> findByAbiertaTrueAndFechaLimiteBefore(LocalDateTime ahora);
 }
